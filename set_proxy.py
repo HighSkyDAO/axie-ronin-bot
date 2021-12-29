b945591e664bf44755a5586a10cf0cdd702fbe7e
vforvoid <v.polosatov@gmail.com>
2021-12-29 13:43:26 +0400

    Minor code improvements for set_proxy.py

--- set_proxy.py
+++ set_proxy.py
@@ -6,8 +6,8 @@ if not os.path.isfile(credentials_filename):
     print("credentials.json file doesn't exist, just run a normal setup: sudo sh scripts/run_setup.sh")
     exit(1)
 
-with open(credentials_filename) as json_file:
-    credentials = json.load(json_file)
+with open(credentials_filename) as credentials_file:
+    credentials = json.load(credentials_file)
 
 proxy = input('Enter your proxy (Example: socks5://user:password@example.com:1080): ')
 if not proxy.strip():
@@ -17,4 +17,4 @@ if not proxy.strip():
 credentials["proxy"] = proxy
 print('Proxy set successfully')
 credentials_dump = json.dumps(credentials, indent=4).encode('utf-8')
-open('credentials.json', 'wb').write(credentials_dump)
+open(credentials_filename, 'wb').write(credentials_dump)
